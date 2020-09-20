7d504f0cb1fd8e859c02fe701fc3e9a313db5982
jm-avila <jm.avila1309@icloud.com>
2020-09-20 14:55:48 +0200

    Connecting to DB from index to ease testing endpoints

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const app = require("./app");
 const port = process.env.PORT;
+require("./mongoose");
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
